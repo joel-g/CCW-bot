1ff5fd95974375f03024302e8ec7162d8ef4807f
Joel Guerra <joelaguerra1@gmail.com>
2018-04-22 21:55:21 -0700

    checks logs for past replies

--- ccwbot.py
+++ ccwbot.py
@@ -9,11 +9,23 @@ def authenticate():
   print('Authenticated as {}\n'.format(reddit.user.me()))
   return reddit
 
-
 def record_commented(submission_id):
   print("Logging comment...")
   with open("commented.txt", 'a+') as comment_log:
     comment_log.write(submission_id + '\n')
+
+def is_replied(submission_id):
+  print("Checking to see if this has already been commented...")
+  sleep(1)
+  readable = open("logs.txt", "r")
+  if submission_id in readable.read().splitlines():
+    print("It has been commented.\n")
+    sleep(1)
+    return True
+  else:
+    print("It has not been commented.\n")
+    sleep(1)
+    return False
 
 def get_submissions(reddit):
   print("Fetching new posts...")
@@ -35,13 +47,14 @@ def get_questions(posts):
 def engine(questions):
   with open('logs.txt','a+') as logs:
     for submission in questions:
-      if submission.id not in logs:
+      print(logs.read().splitlines())
+      if submission.id not in logs.read().splitlines():
         try:
-          submission.reply("It looks like you asked a question in /r/CCW. There's a good chance you're asking what gun you should buy/carry. The answer is a Glock 19.")
+          # submission.reply("It looks like you asked a question in /r/CCW. There's a good chance you're asking what gun you should buy/carry. The answer is a Glock 19.")
           print("Replied to '" + submission.title + "'")
           logs.write(str(submission.id) + "\n")
           print("Sleeping 500 seconds")
-          sleep(500)
+          sleep(5)
         except:
           "Couldn't reply for some reason"
       sleep
